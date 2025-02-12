9b1a491e60aaeb8dd57f32849f68dadba749c461
vikas-nwic <vikas.nwic@gmail.com>
2025-02-12 18:11:43 +0530

    Changes job handler and properties config etc

---
+++ src/main/java/com/nwic/gt/decoder/file/tracking/system/agencies/TelanganaSurfaceWaterAgency.java
@@ -0,0 +1,8 @@
+package com.nwic.gt.decoder.file.tracking.system.agencies;/**
+ * Author: Vikas Predhva
+ * Designation : Software Engineering
+ * Organization: Grant Thornton
+ * Date: 12-02-2025
+ * Description: test Description
+ */public class TelanganaSurfaceWaterAgency {
+}
